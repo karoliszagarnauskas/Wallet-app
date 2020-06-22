e0939d6e9a13105be6f4c8d6a716985615f61a15
karoliszagarnauskas <35958904+karoliszagarnauskas@users.noreply.github.com>
2020-06-23 01:49:00 +0300

    3rd

--- main.js
+++ main.js
@@ -1,29 +1,44 @@
 console.log("veikiu");
-// var today = new Date().toISOString().substr(0, 10);
-// document.querySelector("#today").value = today;
+
 
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var b3 = $("#b3"), b4 = $("#b4");
+$('#b4,#b3').click(function(){
+    console.log('clicked2', this.id);
+});
 
-function addMoney() {
-  var field  = document.querySelector("#field").value;
-  console.log(field);
-  if(document.getElementById('select').value == "Natural") {
-  document.querySelector("footer").innerHTML += "Money added:"+" -"+ field +"," + " Natural person" +" ,"+'<br>'+date+ "<br>";
+  $('#b4, #b3').click(function () {
+  if (this.id == 'b3') {
+  var jsonObj = $('#my-form').serializeArray();
+  $('#out').val(JSON.stringify(jsonObj) +($('select').val())+" "+($('#b3').val())  );
+  // $('#outSelect').val(JSON.stringify($('select').val()));
+  // $('#out').val(JSON.stringify(jsonObj) +($('input').val()));
+}else if (this.id == 'b4') {
+  var jsonObj = $('#my-form').serializeArray();
+  $('#out').val(JSON.stringify(jsonObj) +($('select').val())+" "+($('#b4').val())  );
 }
-if(document.getElementById('select').value == "Legal"){
-  document.querySelector("footer").innerHTML += "Money added:"+" -"+ field +"," + " Legal person" +" ,"+date+ "<br>";
-}
-}
-function withdrawMoney() {
-  var field  = document.querySelector("#field").value;
-  console.log(field);
-  if(document.getElementById('select').value == "Natural") {
-  document.querySelector("footer").innerHTML += "Money withdrawn:"+" -"+ field +"," + " Natural person" +" ,"+date+ "<br>";
-}
-if(document.getElementById('select').value == "Legal"){
-  document.querySelector("footer").innerHTML += "Money withdrawn:"+" -"+ field +"," + " Legal person" + " ,"+date+ "<br>";
-}
-}
+});
 
-// document.write(today);
+// document.querySelectorAll('.current').forEach(item => {
+//   item.addEventListener('click', event => {
+//     const data = getData('#my-form');
+//     console.log(data);
+//     console.log(JSON.stringify(data));
+//   })
+// })
+// console.log(today);
+//
+// var myJSON = {
+// 	"date": "today",
+// 	"user_id": 1,
+// 	"value": "Natural",
+// 	"Add Money": {
+// 		"Amount": 200.00,
+//     "Currency": "Eur"
+// 	}
+// };
+// convertedtostring = JSON.stringify(myJSON);
+// console.log(convertedtostring);
+//
+// document.querySelector('footer').innerHTML += convertedtostring;
