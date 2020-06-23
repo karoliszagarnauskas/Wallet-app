7f14385b34bd02b69649eae34e79316165c9b0df
karoliszagarnauskas <35958904+karoliszagarnauskas@users.noreply.github.com>
2020-06-24 02:03:16 +0300

    4th

--- main.js
+++ main.js
@@ -12,13 +12,50 @@ $('#b4,#b3').click(function(){
   if (this.id == 'b3') {
   var jsonObj = $('#my-form').serializeArray();
   $('#out').val(JSON.stringify(jsonObj) +($('select').val())+" "+($('#b3').val())  );
+  if (document.getElementById('field').value > 16667) {
+  var x = document.getElementById('field').value;
+   y = x*0.03/100;
+  if (y>5) {
+    console.log(5);
+  // console.log($('#out').val());
   // $('#outSelect').val(JSON.stringify($('select').val()));
   // $('#out').val(JSON.stringify(jsonObj) +($('input').val()));
+}
+}else if (document.getElementById('field').value < 16667) {
+  var x = document.getElementById('field').value;
+  y = x*0.03/100;
+
+  console.log(Math.ceil(y * 100) / 100 );
+}
 }else if (this.id == 'b4') {
   var jsonObj = $('#my-form').serializeArray();
   $('#out').val(JSON.stringify(jsonObj) +($('select').val())+" "+($('#b4').val())  );
+  if (document.getElementById('field').value) {
+  var x = document.getElementById('field').value;
+   y = x*0.3/100;
+
+   console.log(Math.ceil(y * 100) / 100 );
+
 }
-});
+
+// else if (document.getElementById('field').value < 16667) {
+//   var x = document.getElementById('field').value;
+//   y = x*0.03/100;
+//
+//   console.log(Math.ceil(y * 100) / 100 );
+// }
+}});
+
+
+
+//cash-in
+// if (($('select').val()) > 16666) {
+// var math = ($('select').val()) * 0.3/100;
+// console.log(math);
+// }else {
+//   // var math = 5;
+//   // console.log(math);
+// }
 
 // document.querySelectorAll('.current').forEach(item => {
 //   item.addEventListener('click', event => {
